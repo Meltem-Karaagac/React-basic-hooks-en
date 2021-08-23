6d83af273cc65083fa2cb9776e73cbbb4a3351a3
elon21 <elongated21@gmail.com>
2021-08-24 02:49:27 +0300

    useState

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class ClassComponent extends React.Component{
+    // constructor(){
+    //     super();
+    //     this.state = {
+    //         count: 0,
+    //     }
+    //     // this.increase = this.increase.bind(this);
+    // }
+
+    state = {
+        count: 0,
+    }
+
+    increase = () => {
+        this.setState({count: this.state.count + 1})
+    }
+
+
     render() {
         return (
             <div className="class">
                 <h2>Class Component</h2>
+                <p>Count: {this.state.count}</p>
+                <button onClick={this.increase}>Increse</button>
             </div>
         )
     }
--- src/FunctionalComponent.js
+++ src/FunctionalComponent.js
@@ -1,8 +1,39 @@
+import {useState} from 'react';
 
 const FunctionalComponent = () => {
+    const [count, setCount] = useState(0);
+    const [age, onChangeAge] = useState(20);
+    const [user, setUser] = useState({
+        name: 'Elon',
+        age: '30',
+        email: 'elon@mail.com'
+    })
+
+    // const increase = () => {
+    //     setCount(count+1);
+    // }
+
+    const increase = () => setCount(count+1);
+
+    // const increaseAge = () => {
+    //     onChangeAge(age+1)
+    // }
+
+    const changeUser = () => {
+        setUser({...user, name: 'Gated'})
+    }
+
     return (
         <div className="function">
             <h2>Functional Component</h2>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count+1)}>Increase</button>
+            {/* <p>Age: {age}</p>
+            <button onClick={increaseAge}>Increase Age</button> */}
+            <p>Name: {user.name}</p>
+            <p>Age: {user.age}</p>
+            <p>Email: {user.email}</p>
+            <button onClick={changeUser}>Change User</button>
         </div>
     )
 }
