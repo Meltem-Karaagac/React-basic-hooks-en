7c4b92826eb02a202cf2640f2e1c84fe19f38caf
elon21 <elongated21@gmail.com>
2021-08-24 04:22:49 +0300

    axios get (second way)

--- src/Users.js
+++ src/Users.js
@@ -5,22 +5,30 @@ const Users = () => {
 
     const [users, setUsers] = useState([]);
 
+    useEffect(() => {
+        // axios.get('https://jsonplaceholder.typicode.com/users')
+        // .then((response) => setUsers(response.data))
 
-    const getUsers = () => {
-        axios.get('https://jsonplaceholder.typicode.com/users')
-        .then((response) => setUsers(response.data))
-    }
+        // axios({
+        //     url: 'https://jsonplaceholder.typicode.com/users',
+        //     method: 'GET'
+        // })
+        // .then((response) => setUsers(response.data))
+
+        // axios('https://jsonplaceholder.typicode.com/users')
+        // .then((response) => setUsers(response.data))
+    },[])
 
 
 
     return (
         <div className="users">
             <h2>Users</h2>
-            <button onClick={getUsers}>Get Users</button>
+            {/* <button onClick={null}>Get Users</button> */}
             <div className="users-cards">
             {users.map((user) => {
                 return(
-                    <div className="useritem">
+                    <div className="useritem" key={user.id}>
                         <img src={`https://avatars.dicebear.com/v2/avataaars/${user.id}.svg`} alt="" />
                         {user.name}
                     </div>
